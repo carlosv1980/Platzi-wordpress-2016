8115b98625cc3de1c865c33eecb8e446fd680a34
Carlos Vudu <carlos@voodooroom.net>
2016-08-16 11:06:19 +0200

    Category - plantilla creada

--- category.php
+++ category.php
@@ -1,19 +1,13 @@
 <?php get_header(); ?>
 <body>
-  <h1 class="page-heading max-width"><?php bloginfo(name); ?></h1>
+  <h1 class="page-heading max-width">Categoría: <?php single_cat_title(); ?></h1>
   <div class="grid max-width">
-    <div class="block grid--item-9">
-      <div class="block__title">
-        Últimos Trabajos
-      </div>
+    <div class="block grid--item-12">
       <div class="block__body grid">
           <?php
-          $args = array('author' => "vuducito");
-          $filter_posts = new WP_Query($args);
-
-          if ($filter_posts->have_posts()) :
-            while ($filter_posts->have_posts()) :
-              $filter_posts->the_post();
+          if (have_posts()) :
+            while (have_posts()) :
+              the_post();
            ?>
               <article class="block grid--item-4">
               <h2 class="block__title"><?php the_title(); ?></h2>
@@ -21,7 +15,7 @@
               <p><?php the_excerpt(); ?></p>
               <footer>
                 <div class="">
-                  <small><?php the_category(); ?></small>
+                  <small><?php the_tags(); ?></small>
                 </div>
                 <div class="">
                   <strong><?php the_author(); ?></strong>
@@ -39,6 +33,5 @@
             ?>
       </div>
     </div>
-    <?php get_sidebar(); ?>
   </div>
   <?php get_footer(); ?>
